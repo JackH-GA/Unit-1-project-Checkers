d05a637abdd06865182962a7ff5e8be0b701ff68
JackH <jwhooson@gmail.com>
2023-12-01 22:07:07 -0800

    chekers array

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log('script is running')
 
+let checkArr = [
+    [c0r0, c1r0, c2r0, c3r0, c4r0, c5r0, c6r0, c7r0]
+    [c0r1, c1r1, c2r1, c3r1, c4r1, c5r1, c6r1, c7r1]
+    [c0r2, c1r2, c2r2, c3r2, c4r2, c5r2, c6r2, c7r2]
+    [c0r3, c1r3, c2r3, c3r3, c4r3, c5r3, c6r3, c7r3]
+    [c0r4, c1r4, c2r4, c3r4, c4r4, c5r4, c6r4, c7r4]
+    [c0r5, c1r5, c2r5, c3r5, c4r5, c5r5, c6r5, c7r5]
+    [c0r6, c1r6, c2r6, c3r6, c4r6, c5r6, c6r6, c7r6]
+    [c0r7, c1r7, c2r7, c3r7, c4r7, c5r7, c6r7, c7r7]
+]
+
