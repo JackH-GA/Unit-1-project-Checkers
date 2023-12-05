4396868841bf17da508eae291732513d95a4d1f9
JackH <jwhooson@gmail.com>
2023-12-05 09:18:17 -0800

    defined constants in js

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 console.log('script is running')
-const checkBoard = document.querySelector("#checkBoard")
+// these constants will be used for 
+const checkBoard = document.getElementsByClassName("checkBoard")
+    // console.log(checkBoard)
 const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 
-let checkArr = [
-    [c0r0, c1r0, c2r0, c3r0, c4r0, c5r0, c6r0, c7r0]
-    [c0r1, c1r1, c2r1, c3r1, c4r1, c5r1, c6r1, c7r1]
-    [c0r2, c1r2, c2r2, c3r2, c4r2, c5r2, c6r2, c7r2]
-    [c0r3, c1r3, c2r3, c3r3, c4r3, c5r3, c6r3, c7r3]
-    [c0r4, c1r4, c2r4, c3r4, c4r4, c5r4, c6r4, c7r4]
-    [c0r5, c1r5, c2r5, c3r5, c4r5, c5r5, c6r5, c7r5]
-    [c0r6, c1r6, c2r6, c3r6, c4r6, c5r6, c6r6, c7r6]
-    [c0r7, c1r7, c2r7, c3r7, c4r7, c5r7, c6r7, c7r7]
-]
+//these constants below allow me to have access to the divs i created in html in this js//
+//I want to be able to access the id of each element within the allSquares array so that I can drag an element to a new div and have it change its location to the new id// 
+const p2Pieces = document.getElementsByClassName("p2Piece")
+    console.log(p2Pieces)
+// this const contains an array of player twos pieces
+const p1Pieces = document.getElementsByClassName("p1Piece")
+console.log(p1Pieces)
+// this const contains an array of player ones pieces
+const allSquares = document.getElementsByClassName("square")
+    console.log(allSquares)
+//this const contains all the 64 squares of the gameboard and their child elements (player pieces)
 
-function createPiece() {
-    
-}
+// function drag() {
+//     allSquares.forEach(square => { 
+//         square.addEventListener('dragpiecestart', dragPieceStart)
+//     })
+// }
 
+
+// function dragPieceStart(e) {
+//     startPieceId = e.target.getAttribute('square-id')
+//     dragElement = e.target
+//     console.log(e)
+// }
+
+ 
+
+
+
+//  A move consists of moving a piece diagonally to an adjacent unoccupied square.
+//  If the adjacent square contains an opponent's piece, 
+//  and the square immediately beyond it is vacant, 
+//  the piece may be captured (and removed from the game) by jumping over it.//
+
+//////***********pseudocode***********/////////
+//create a function that allows me to iterate board positions and ids into an array
