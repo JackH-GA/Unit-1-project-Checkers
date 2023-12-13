87e9a4cab7e4d94bc4600307cead57a65a61ff93
JackH <jwhooson@gmail.com>
2023-12-12 16:08:49 -0800

    rewrites the functions for onclick

--- script.js
+++ script.js
@@ -42,93 +42,93 @@ function dragOver(e) {
     console.log(e.target);
 }
 
-function dragDrop(e) {
-    e.stopPropagation();
-    console.log(e.target);
-    e.target.append(dragElement);
-    const correctTurn = dragElement.classList.contains(playerTurn);
-    const taken = e.target.classList.contains('piece');
-    const legal = checkIfLegal(e.target);
-    const opponentTurn = playerTurn === 'black' ? 'white' : 'black';
-    const takenByOpponent = e.target.classList.contains(opponentTurn);
+// function dragDrop(e) {
+//     e.stopPropagation();
+//     console.log(e.target);
+//     e.target.append(dragElement);
+//     const correctTurn = dragElement.classList.contains(playerTurn);
+//     const taken = e.target.classList.contains('piece');
+//     const legal = checkIfLegal(e.target);
+//     const opponentTurn = playerTurn === 'black' ? 'white' : 'black';
+//     const takenByOpponent = e.target.classList.contains(opponentTurn);
 
-    if (correctTurn) {
-        if (takenByOpponent && legal) {
-            e.target.parentNode.append(dragElement);
-            e.target.remove();
-            changePlayer();
-            return;
-        }
-        if (taken && !takenByOpponent) {
-            return;
-        }
-        if (legal) {
-            e.target.append(dragElement);
-            changePlayer();
-            return;
-        }
-    }
-}
+//     if (correctTurn) {
+//         if (takenByOpponent && legal) {
+//             e.target.parentNode.append(dragElement);
+//             e.target.remove();
+//             changePlayer();
+//             return;
+//         }
+//         if (taken && !takenByOpponent) {
+//             return;
+//         }
+//         if (legal) {
+//             e.target.append(dragElement);
+//             changePlayer();
+//             return;
+//         }
+//     }
+// }
 
-function changePlayer() {  
-    if (playerTurn === 'white') {
-        reverseIds();
-        playerTurn = 'black';
-    } else {
-        revertIds();
-        playerTurn = 'white';
-    }
-}
+// function changePlayer() {  
+//     if (playerTurn === 'white') {
+//         reverseIds();
+//         playerTurn = 'black';
+//     } else {
+//         revertIds();
+//         playerTurn = 'white';
+//     }
+// }
 
-function reverseIds() {
-    const allSquares = document.querySelectorAll(".square");
-    allSquares.forEach((square, i) => 
-        square.setAttribute('square-id', (width * width -1) - i));
-}
+// function reverseIds() {
+//     const allSquares = document.querySelectorAll(".square");
+//     allSquares.forEach((square, i) => 
+//         square.setAttribute('square-id', (width * width -1) - i));
+// }
 
-function revertIds() {
-    const allSquares = document.querySelectorAll(".square");
-    allSquares.forEach((square, i) => square.setAttribute('square-id', i));
-}
+// function revertIds() {
+//     const allSquares = document.querySelectorAll(".square");
+//     allSquares.forEach((square, i) => square.setAttribute('square-id', i));
+// }
 
-function checkIfLegal(target) {
-    const targetId = Number(target.getAttribute('square-Id')) || Number(target.parentNode.getAttribute('square-id'));
-    const startId = Number(startPieceId);
-    const piece = dragElement.id;
-}
+// function checkIfLegal(target) {
+//     const targetId = Number(target.getAttribute('square-Id')) || Number(target.parentNode.getAttribute('square-id'));
+//     const startId = Number(startPieceId);
+//     const piece = dragElement.id;
+// }
 
-function pieceMoves() {
-    if (playerTurn === 'white') {
-        startId + width - 1 === targetId ||
-        startId + width + 1 === targetId 
-    } else {
-        startId - width - 1 === targetId ||
-        startId - width + 1 === targetId 
-    }
-}
+// function pieceMoves() {
+//     if (playerTurn === 'white') {
+//         startId + width - 1 === targetId ||
+//         startId + width + 1 === targetId 
+//     } else {
+//         startId - width - 1 === targetId ||
+//         startId - width + 1 === targetId 
+//     }
+// }
 
-function kingMoves() { 
-    if (p1Pieces || p2Pieces === 'piece.p1.k' || 'piece.p2.k' ) {
-        startId + width - 1 === targetId ||
-        startId + width + 1 === targetId ||
-        startId - width - 1 === targetId ||
-        startId - width + 1 === targetId
-    }
-} 
+// function kingMoves() { 
+//     if (p1Pieces || p2Pieces === 'piece.p1.k' || 'piece.p2.k' ) {
+//         startId + width - 1 === targetId ||
+//         startId + width + 1 === targetId ||
+//         startId - width - 1 === targetId ||
+//         startId - width + 1 === targetId
+//     }
+// } 
 
-function makeKingP2() {
-    if (p2Pieces.id === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7) {
-        p2Pieces.append(".k")
-    }
-}
+// function makeKingP2() {
+//     if (p2Pieces.id === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7) {
+//         p2Pieces.append(".k")
+//     }
+// }
 
-function makeKingP1() {
-    if (p1Pieces.id === 63 || 62 || 61 || 60 || 59 || 58 || 57 || 56) {
-        p1Pieces.append(".k")
-    }
-}
+// function makeKingP1() {
+//     if (p1Pieces.id === 63 || 62 || 61 || 60 || 59 || 58 || 57 || 56) {
+//         p1Pieces.append(".k")
+//     }
+// }
 
-//  A move consists of moving a piece diagonally to an adjacent unoccupied square.
-//  If the adjacent square contains an opponent's piece, 
-//  and the square immediately beyond it is vacant, 
-//  the piece may be captured (and removed from the game) by jumping over it.//
+// //  A move consists of moving a piece diagonally to an adjacent unoccupied square.
+// //  If the adjacent square contains an opponent's piece, 
+// //  and the square immediately beyond it is vacant, 
+// //  the piece may be captured (and removed from the game) by jumping over it.//
